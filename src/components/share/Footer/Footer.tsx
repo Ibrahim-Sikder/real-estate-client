f610effde629247801f641bb61968c33960ef2b1
liton61 <litonofficial61@gmail.com>
2024-09-26 15:51:24 +0600

    header and footer added

--- src/components/share/Footer/Footer.tsx
+++ src/components/share/Footer/Footer.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import logo from "../../../assets/images/logo/Kalam_s-Tutorial.png";
-import softyPy from "../../../../src/assets/images/logo/softy.png";
-import pay from "../../../../src/assets/images/logo/pay.png";
+import logo from "../../../../src/assets/images/logo/logo.png";
+import softy from "../../../../src/assets/images/logo/softy.png";
 import Container from "../Container";
 
 import FacebookIcon from "@mui/icons-material/Facebook";
@@ -23,13 +22,8 @@ const Footer = () => {
             {/* Social Media Icons */}
             <div className="flex lg:justify-start justify-center">
               <div className="lg:mt-0 mt-10">
-                <div className="flex items-center space-x-2">
-                  <Image src={logo} alt="Seville Logo" width={50} height={50} />
-                  <h3 className="font-bold text-lg">Kalam&apos;s Tutorial</h3>
-                </div>
-                <h4 className="font-semibold text-2xl mt-5">
-                  আমাদের সাথে যুক্ত হও
-                </h4>
+                <Image src={logo} alt="" className="w-28" />
+                <h4 className="mt-5">Connect Wit Us</h4>
                 <ul className="flex lg:justify-start justify-center gap-5 mt-3">
                   <li>
                     <Link href="#">
@@ -58,19 +52,16 @@ const Footer = () => {
             {/* contact */}
             <div className="flex lg:justify-start justify-center">
               <div className="lg:mt-0 mt-10">
-                <h4 className="font-semibold text-2xl mb-3 lg:text-start text-center">
-                  যোগাযোগ
-                </h4>
+                <h4 className="mb-3 lg:text-start text-center">Contact</h4>
                 <ul className="space-y-3 lg:text-start text-center">
                   <li>
-                    <CallIcon /> 01601929244
+                    <CallIcon /> 01XXXXXXXXXX
                   </li>
                   <li>
-                    <MailOutlineIcon /> fahadstutorial@gmail.com
+                    <MailOutlineIcon /> example@gmail.com
                   </li>
                   <li>
-                    <LocationOnIcon /> 10/B, Plot: 03, Fakirbari, Mirpur, Dhaka
-                    - 1216
+                    <LocationOnIcon /> Dhaka, Bangladesh
                   </li>
                 </ul>
               </div>
@@ -79,42 +70,29 @@ const Footer = () => {
             {/* Our Services */}
             <div className="flex lg:justify-start justify-center">
               <div className="lg:mt-0 mt-10">
-                <h4 className="font-semibold lg:text-start text-center">
-                  কোম্পানি
-                </h4>
+                <h4 className="lg:text-start text-center">Company</h4>
                 <ul className="mt-3 space-y-2 lg:text-start text-center">
                   <Link href="/faq">
-                    <li>সচরাচর জিজ্ঞাসিত প্রশ্ন</li>
+                    <li>FAQ</li>
                   </Link>
                   <Link href="/about">
-                    <li>আমাদের সম্পর্কে</li>
+                    <li>About us</li>
                   </Link>
                   <Link href="/terms">
-                    <li>টার্মস এবং শর্তাবলি</li>
+                    <li>Terms & Condition</li>
                   </Link>
                   <Link href="/privacy">
-                    <li>প্রাইভেসি পলিসি</li>
-                  </Link>
-                  <Link href="/refund">
-                    <li>রিফান্ড পলিসি</li>
+                    <li>Privacy Policy</li>
                   </Link>
                 </ul>
               </div>
             </div>
           </div>
-
+          <hr className="mt-5" />
           {/* Bottom Section */}
-          <div className="mt-8 text-center">
-            <div className="my-5">
-              <Image src={pay} alt="" className="w-full" />
-            </div>
-            <p>
-              &copy; Copyrights Kalam&apos;s Tutorial 2024. All Rights Reserved
-            </p>
-            <div className="flex justify-center items-center space-x-5 mt-3 lg:w-64 md:w-64 w-full mx-auto rounded">
-              <p>Developed by</p>
-              <Image src={softyPy} alt="" className="w-24" />
-            </div>
+          <div className="flex justify-center items-center space-x-5 mt-5 lg:w-64 md:w-64 w-full mx-auto rounded">
+            <p className="text-center">Developed by</p>
+            <Image src={softy} alt="" className="w-24" />
           </div>
         </Container>
       </footer>
--- src/components/share/Slider/Slider.tsx
+++ src/components/share/Slider/Slider.tsx
@@ -10,58 +10,53 @@ import "swiper/css/zoom";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 
 import Image from "next/image";
-import img1 from "../../../assets/images/courses/course (1).jpg";
-import img2 from "../../../assets/images/courses/course (2).jpg";
-import img3 from "../../../assets/images/courses/course (3).jpg";
+import img1 from "../../../assets/images/banner/banner.jpg";
 
 import "./Slider.css";
-import Container from "../Container";
 
 const slides = [
   {
     img: img1,
   },
   {
-    img: img2,
+    img: img1,
   },
   {
-    img: img3,
+    img: img1,
   },
 ];
 
 const Slider = () => {
   return (
     <>
-      <Container>
-        <div className="slider-container py-4">
-          <Swiper
-            autoplay={{
-              delay: 6000,
-              disableOnInteraction: false,
-            }}
-            spaceBetween={0}
-            navigation={true}
-            pagination={{
-              clickable: true,
-            }}
-            modules={[Autoplay, Navigation, Pagination]}
-            className="mySwiper"
-          >
-            {slides.map((slide, index) => (
-              <SwiperSlide key={index} className="">
-                <div className="relative w-full h-auto md:h-[400px] lg:h-[600px]">
-                  <Image
-                    src={slide.img}
-                    alt={`Slide ${index + 1}`}
-                    objectFit="cover"
-                    className="slide-image h-auto md:h-[400px] lg:h-[600px]"
-                  />
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
-      </Container>
+      <div className="slider-container">
+        <Swiper
+          autoplay={{
+            delay: 6000,
+            disableOnInteraction: false,
+          }}
+          spaceBetween={0}
+          navigation={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Autoplay, Navigation, Pagination]}
+          className="mySwiper"
+        >
+          {slides.map((slide, index) => (
+            <SwiperSlide key={index} className="">
+              <div className="relative w-full h-auto md:h-[400px] lg:h-[600px]">
+                <Image
+                  src={slide.img}
+                  alt={`Slide ${index + 1}`}
+                  objectFit="cover"
+                  className="slide-image h-auto md:h-[400px] lg:h-[600px]"
+                />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </>
   );
 };

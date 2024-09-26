cb913f92875d171eee80395016efe9a538090748
liton61 <litonofficial61@gmail.com>
2024-09-26 17:51:02 +0600

    about section added

--- src/app/(mainLayout)/page.tsx
+++ src/app/(mainLayout)/page.tsx
@@ -1,13 +1,14 @@
 "use client";
+import About from "@/components/About/About";
 import Slider from "@/components/share/Slider/Slider";
 
 import React from "react";
 
-
 const Home = () => {
   return (
     <div>
-      <Slider />      
+      <Slider />
+      <About />
     </div>
   );
 };

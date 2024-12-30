dccf5d8b4498f01cf07b7ea3cf94ac14e4c49c72
Ibrahim Sikder <ibrahimsikder5033@gmail.com>
2024-12-30 16:54:20 +0600

    lottie-remove

--- src/components/share/Loader/Loader.tsx
+++ src/components/share/Loader/Loader.tsx
@@ -1,17 +1,30 @@
-"use client";
-import { Player } from "@lottiefiles/react-lottie-player";
-import loaderAnimation from "../../../../src/assets/loader/loader.json";
+// "use client";
+// import { Player } from "@lottiefiles/react-lottie-player";
+// import loaderAnimation from "../../../../src/assets/loader/loader.json";
+
+// const Loader = () => {
+//   return (
+//     <div className="fixed inset-0 h-screen flex items-center justify-center bg-white z-50">
+//       <Player
+//         autoplay
+//         loop
+//         src={loaderAnimation}
+
+//         className="w-[150px] h-[150px] md:w-[200px] md:h-[150px]"
+//       />
+//     </div>
+//   );
+// };
+
+// export default Loader;
+
+
+import React from 'react';
 
 const Loader = () => {
   return (
-    <div className="fixed inset-0 h-screen flex items-center justify-center bg-white z-50">
-      <Player
-        autoplay
-        loop
-        src={loaderAnimation}
-
-        className="w-[150px] h-[150px] md:w-[200px] md:h-[150px]"
-      />
+    <div>
+      <h4>Loading.......</h4>
     </div>
   );
 };
